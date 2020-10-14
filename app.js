0386821f6d4b4546a106922f285e4888e970f8cf
Berezin Konstantin <berezinko90@gmail.com>
2020-10-14 21:24:46 +0700

    lead-up

---
+++ app.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const { errors } = require('celebrate');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/', { // put db name
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(limiter);
+app.use(helmet());
+app.use(bodyParser.json());
+
+app.use(requestLogger);
+
+// Routes
+
+app.use(errorLogger);
+
+app.use(errors()); // обработчик ошибок celebrate
+
+// Централизованный обработчик ошибок
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
