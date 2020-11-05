6775cbc2788fda5bcea4f45d662bfa3480285dd8
Berezin Konstantin <berezinko90@gmail.com>
2020-11-05 21:31:00 +0700

    add CORS settings

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const rateLimiter = require('./middlewares/rateLimiter');
 const errorHandler = require('./middlewares/errorHahdler');
 const { DATA_BASE } = require('./constants/config');
 
+const allowedCors = [
+  'http://bko-news.students.nomoreparties.xyz',
+  'https://bko-news.students.nomoreparties.xyz',
+  'localhost:3000',
+];
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -26,6 +31,15 @@ app.use(helmet());
 app.use(bodyParser.json());
 
 app.use(requestLogger);
+
+// CORS
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+});
 
 app.use('/', router);
 
