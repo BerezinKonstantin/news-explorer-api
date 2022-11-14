ff4729455eab0c6228fc6d4685bd91bc60d117f4
KonstantinBerezin <berezinko90@gmail.com>
2022-11-14 23:03:27 +0500

    cors settings

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
-const cors = require("cors");
 const { errors } = require("celebrate");
 
 const router = require("./routes");
@@ -13,6 +12,7 @@ const errorHandler = require("./middlewares/errorHahdler");
 const { DATA_BASE } = require("./constants/config");
 
 const PORT = process.env.PORT || 3001;
+const allowedCors = ["https://berezinkonstantin.github.io", "localhost:3000"];
 const app = express();
 
 mongoose.connect(DATA_BASE, {
@@ -27,16 +27,28 @@ app.use(bodyParser.json());
 
 app.use(requestLogger);
 
-app.use(
-  cors({
-    origin: [
-      "https://berezinkonstantin.github.io/",
-      "https://berezinkonstantin.github.io/news-explorer-frontend/",
-      "https://berezinkonstantin.github.io/saved-news/",
-    ],
-    credentials: true,
-  })
-);
+app.use(function (req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  // сохраняем список заголовков исходного запроса
+  const requestHeaders = req.headers["access-control-request-headers"];
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === "OPTIONS") {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+  next();
+});
 
 app.use("/", router);
 
